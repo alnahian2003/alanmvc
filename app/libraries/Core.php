3a6545b2953f282684e00404fcb86f74f258d8c1
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 01:05:42 +0600

    mvc core.php updated

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -8,13 +8,28 @@
 
 class Core
 {
-    protected $currentController = "Pages";
+    protected $currentController = "Pages"; // "Pages" is the default controller
     protected $currentMethod = "index";
     protected $params = [];
 
     public function __construct()
     {
-        print_r($this->getUrl());
+        // print_r($this->getUrl());
+        $url = $this->getUrl();
+
+        // look in controllers for first value
+        $controllersPath = "../app/controllers/";
+        $fileName = $controllersPath . ucfirst($url[0]);
+
+        if (file_exists($fileName)) {
+            // If exists, set as controller
+            $this->currentController = ucfirst($url[0]);
+            // Unset 0 Index of the url array
+            unset($url[0]);
+        }
+
+        // Require the controller
+        require_once $controllersPath . $this->$currentController . "php";
     }
 
     // Method to get the url
