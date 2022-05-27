a880ae6f29d83362548e764479ff6b588fb78214
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 17:01:19 +0600

    Execute the prepared statement method created

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -65,4 +65,9 @@ class Database
             $this->stmt->bindValue($param, $value, $type);
         }
     }
+    // Execute the prepared statement
+    public function execute()
+    {
+        return $this->stmt->execute();
+    }
 }
