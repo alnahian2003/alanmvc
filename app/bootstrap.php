a5ee1f08cd47674953c60c530c3b4a906bd44705
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 12:52:36 +0600

    Core libs autoloaded

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,11 +3,6 @@
 // Load Config
 require_once "config/config.php";
 
-// Load all the Libraries
-require_once "libraries/Core.php";
-require_once "libraries/Controller.php";
-require_once "libraries/Database.php";
-
 // Autoload Core LIbraries
 spl_autoload_register(function ($className) {
     require_once "libraries/{$className}.php";
