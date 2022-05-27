4f4d3c81bcbd9403a57187567def4b5ebf701d27
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 12:51:25 +0600

    autoloader added, concatination removed in case of good practice

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,3 +7,8 @@ require_once "config/config.php";
 require_once "libraries/Core.php";
 require_once "libraries/Controller.php";
 require_once "libraries/Database.php";
+
+// Autoload Core LIbraries
+spl_autoload_register(function ($className) {
+    require_once "libraries/{$className}.php";
+});
