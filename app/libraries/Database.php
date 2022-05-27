18707b9bbaaf530295e6db67a04df90024fc8da3
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 16:13:34 +0600

    database class created and basic configuration completed

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,1 +1,30 @@
-<?php  ?>
+<?php
+/*
+ * PDO Database Class
+ * It connects to database 
+ * Creates prepared statements
+ * Bind values
+ * return/fetch rows and results
+ */
+class Database
+{
+    private $host = DB_HOST;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $dbname = DB_NAME;
+
+    private $dbh; //db handler prop
+    private $stmt; //statement
+    private $err; //error
+
+    public function __construct()
+    {
+        // Set DSN
+        $dsn = "mysql:host={$this->host}; dbname={$this->dbname}";
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+    }
+}
