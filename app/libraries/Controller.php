0db0ed56b9ec098b6c76ba660f2487b51262a76e
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 12:14:33 +0600

    model method adde to Controller class

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,1 +1,19 @@
-<?php  ?>
+<?php
+
+/**
+ * This is the Base Controller
+ * This loads the Models and Views 
+ */
+
+class Controller
+{
+    // Load Model
+    public function model($model)
+    {
+        // Require model file
+        require_once "../app/models/" . $model . ".php";
+
+        // Instantiate the model
+        return new $model();
+    }
+}
