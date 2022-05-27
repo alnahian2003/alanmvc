24d7e0b749011869d10159ad254ee961fa12c86f
alnahian2003 <a.alnahian2003@gmail.com>
2022-05-27 12:17:31 +0600

    file path renamed, view method added to controller

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -16,4 +16,16 @@ class Controller
         // Instantiate the model
         return new $model();
     }
+    // Load View
+    public function view($view, $data = [])
+    {
+        // Check for view file
+        if (file_exists("../app/views" . $view . ".php")) {
+            // if view exist, then require it
+            require_once "../app/views" . $view . ".php";
+        } else {
+            die("View does not exist");
+        }
+        // return new $view();
+    }
 }
